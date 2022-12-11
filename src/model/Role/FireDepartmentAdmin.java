f80e9fdebc67e7e5302148930c73c70392e27966
SiddhantBurse <114305176+SiddhantBurse@users.noreply.github.com>
2022-12-10 21:51:39 -0500

    added UI for creating fire department

--- src/model/Role/FireDepartmentAdmin.java
+++ src/model/Role/FireDepartmentAdmin.java
@@ -4,10 +4,21 @@
  */
 package model.Role;
 
+import UI.FireDepartment.FireLandingJPanel;
+import model.EcoSystem;
+import model.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+
+
 /**
  *
  * @author dhruv
  */
-public class FireDepartmentAdmin {
+public class FireDepartmentAdmin extends Role{
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, EcoSystem business) {
+        return new FireLandingJPanel(userProcessContainer, account, business);
+    }
     
 }
