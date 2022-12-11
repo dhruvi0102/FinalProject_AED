107d9056e757292add97b20b64b3abe84490d951
dhruvi0102 <bavaria.d@northeastern.edu>
2022-12-10 21:58:09 -0500

    new control unit added

---
+++ src/model/PoliceDepartment/TrafficControlUnit.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.PoliceDepartment;
+
+/**
+ *
+ * @author dhruv
+ */
+public class TrafficControlUnit {
+    private String trafficcontrolUnit;
+    
+    public TrafficControlUnit(String trafficcontrolUnit ){
+        this.trafficcontrolUnit = trafficcontrolUnit;
+    }
+    
+    public String getTrafficcontrolUnit() {
+        return trafficcontrolUnit;
+    }
+
+    public void setTrafficcontrolUnit(String trafficcontrolUnit) {
+        this.trafficcontrolUnit = trafficcontrolUnit;
+    }
+
+}
